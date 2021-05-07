047fd40367296fe75e98fb591e9daf1ed65b7dc3
David Schneck <david.schneckster@hotmail.com>
2021-05-07 13:15:36 -0400

    made it more modular

--- generator.py
+++ generator.py
@@ -13,22 +13,22 @@ def generateAccounts(N):
 
 	return ret
 
-algod_client = None
+def generator():
+	algod_client = None
 
-# You need to make your own config.json with your 
-# ALGOD_TOKEN and ALGOD_ADDRESS given to you by your access nodeo
-with open("config.json", "r") as config:
-	info = json.load(config)
-	algod_client = algod.AlgodClient(info["ALGOD_TOKEN"],info["ALGOD_ADDRESS"])
+	# You need to make your own config.json with your 
+	# ALGOD_TOKEN and ALGOD_ADDRESS given to you by your access nodeo
+	with open("config.json", "r") as config:
+		info = json.load(config)
+		algod_client = algod.AlgodClient(info["ALGOD_TOKEN"],info["ALGOD_ADDRESS"])
 
-file_name = "accounts-{}.json".format(secrets.randbits(32)) 
+	file_name = "accounts-{}.json".format(secrets.randbits(32)) 
 
-with open(file_name, "w") as output:
-	n = int(sys.argv[1])
-	final_object = generateAccounts(n)
-	json.dump(final_object, output, indent=4)
+	with open(file_name, "w") as output:
+		n = int(sys.argv[1])
+		final_object = generateAccounts(n)
+		json.dump(final_object, output, indent=4)
 	
-
-
-
+if __name__ == '__main__':
+	generator()
 
